ecaa8f2c2b327b36ad15225f7d16dc1e9fe3d3ad
ridmall <ridmall@99x.lk>
2019-07-16 23:59:52 +0530

    Change test in selenide POC

--- codeceptPoc/openMRS_patient_registration_test_test.js
+++ codeceptPoc/openMRS_patient_registration_test_test.js
@@ -3,6 +3,9 @@ Feature('OpenMRS patient_registration_test');
 
 Scenario('test something', (I) => {
     I.amOnPage('http://localhost:8090/openmrs');
-    I.see('OpenMRS');
+    I.appendField('username', 'admin');
+    I.appendField('password', 'Admin123');
+    I.click('Outpatient Clinic');
+    I.click('loginButton');
 });
 
