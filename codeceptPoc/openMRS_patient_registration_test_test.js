832eddb5f216266588014c089efdccb4e705ca34
ridmall <ridmall@99x.lk>
2019-07-23 11:40:51 +0530

    update readme file in selenium POC

--- codeceptPoc/openMRS_patient_registration_test_test.js
+++ codeceptPoc/openMRS_patient_registration_test_test.js
@@ -2,10 +2,16 @@
 Feature('OpenMRS patient_registration_test');
 
 Scenario('test something', (I) => {
-    I.amOnPage('http://localhost:8090/openmrs');
+    I.amOnPage('https://demo.openmrs.org/openmrs/login.htm');
     I.appendField('username', 'admin');
     I.appendField('password', 'Admin123');
-    I.click('Outpatient Clinic');
-    I.click('loginButton');
+    I.click('#Outpatient Clinic');
+    I.click('#loginButton');
+
+    I.click('#referenceapplication-registrationapp-registerPatient-homepageLink-referenceapplication-registrationapp-registerPatient-homepageLink-extension')
+    see('Register a patient')
+    
+
+
 });
 
--- seleniumWebDriverPoc/README.md
+++ seleniumWebDriverPoc/README.md
@@ -1,1 +1,5 @@
 ## End to End testing POC with selenium web driver
+
+# run test -  mvn clean test -Dtest=PatientRegistrationTest
+
+# used chrome web driver for testing ( Add web driver path to webDriverStartup.java class )
